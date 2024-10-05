5dd6729b99d553777e50ca1f0649753466b23029
wilson <wilsonliu202@gmail.com>
2024-10-05 13:41:40 -0400

    Add login and signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import NavBar from "./components/NavBar";
+import Signup from "./components/profile/Signup";
+import Login from "./components/profile/Login";
 
 export default function App() {
   return (
     <>
       <NavBar />
+      <Signup />
     </>
   );
 }
