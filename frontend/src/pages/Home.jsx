575f748536c16d564727b7cc9c253dc64e4e13af
wilson <wilsonliu202@gmail.com>
2024-10-06 01:17:03 -0400

    Ui changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import FeedContent from "../components/home/FeedContent";
 import testimg1 from "../assets/test-images/test-1.jpg";
 import testimg2 from "../assets/test-images/test-2.jpg";
@@ -9,6 +10,7 @@ import NewLocationForm from "../components/home/NewLocationForm";
 
 const Home = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleFormOpen = () => {
     setIsFormOpen(true);
@@ -18,10 +20,18 @@ const Home = () => {
     setIsFormOpen(false);
   };
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
       <LoggedNavbar />
-      <div className="bg-lighter-base-color min-h-screen">
+      <div className="min-h-screen bg-lighter-base-color">
         <div className="mx-auto max-w-screen-lg pt-32">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
             <FeedContent image={testimg1} title="title1" date="1/1/24" />
@@ -31,12 +41,29 @@ const Home = () => {
           </div>
         </div>
         <div className="relative">
-          <button
-            onClick={handleFormOpen}
-            className="fixed bottom-10 right-10 flex h-14 w-14 items-center justify-center rounded-full bg-gray-600 text-white shadow-md transition-all hover:bg-gray-500"
+          <motion.div
+            className="fixed bottom-10 right-10 flex items-center"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
-            +
-          </button>
+            {isHovered && (
+              <motion.span
+                className="mr-4 text-lg font-thin"
+                initial={{ opacity: 0, x: 20 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 20 }}
+                transition={{ duration: 0.2 }}
+              >
+                Add New Location
+              </motion.span>
+            )}
+            <button
+              onClick={handleFormOpen}
+              className="flex h-14 w-14 items-center justify-center rounded-full bg-darker-base-color text-2xl text-white shadow-md transition-all hover:scale-105 hover:opacity-80"
+            >
+              +
+            </button>
+          </motion.div>
         </div>
 
         {isFormOpen && <NewLocationForm onClose={handleFormClose} />}
