33e6c613ad967fe6c5754e286afa833bb4bdf3f1
Crosve Lucero <92947897+crosve@users.noreply.github.com>
2024-10-05 20:01:51 -0400

    jwt changes

--- frontend/src/components/profile/Login.jsx
+++ frontend/src/components/profile/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import NavBar from "../NavBar";
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -26,47 +30,51 @@ const Login = () => {
         setMessage("Sucess");
 
         localStorage.setItem("jwtToken", response.data.jwtToken);
+        navigate("/feed");
       }
     } catch (error) {
-      setError(err.response?.data?.message || "An error occurred");
+      setError(error.response?.data?.message || "An error occurred");
     }
 
     console.log(formData);
   };
 
   return (
-    <div className="bg-lighter-base-color flex h-screen items-center justify-center">
-      <form
-        className="max-w-sm space-y-6 rounded-md bg-white p-8 shadow-md"
-        onSubmit={handleSubmit}
-      >
-        <h2 className="mb-4 text-center text-2xl font-thin">Login</h2>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email or Username"
-          value={formData.email}
-          onChange={handleChange}
-          className="w-full rounded-md border px-4 py-2 shadow-sm"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          className="w-full rounded-md border px-4 py-2 shadow-sm"
-        />
-        <button
-          type="submit"
-          className="bg-base-color w-full rounded-md py-2 text-white shadow-md transition-all duration-300 hover:scale-105"
+    <>
+      <NavBar />
+      <div className="bg-lighter-base-color flex h-screen items-center justify-center">
+        <form
+          className="max-w-sm space-y-6 rounded-md bg-white p-8 shadow-md"
+          onSubmit={handleSubmit}
         >
-          Login
-        </button>
-        {message && <h1 className="text-center text-green-500">{message}</h1>}
-        {error && <h1 className="text-center text-red-600">{error}</h1>}
-      </form>
-    </div>
+          <h2 className="mb-4 text-center text-2xl font-thin">Login</h2>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email or Username"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full rounded-md border px-4 py-2 shadow-sm"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full rounded-md border px-4 py-2 shadow-sm"
+          />
+          <button
+            type="submit"
+            className="bg-base-color w-full rounded-md py-2 text-white shadow-md transition-all duration-300 hover:scale-105"
+          >
+            Login
+          </button>
+          {message && <h1 className="text-center text-green-500">{message}</h1>}
+          {error && <h1 className="text-center text-red-600">{error}</h1>}
+        </form>
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/profile/Signup.jsx
+++ frontend/src/components/profile/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import NavBar from "../NavBar";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -60,55 +61,59 @@ const Signup = () => {
   };
 
   return (
-    <div className="bg-lighter-base-color flex h-screen items-center justify-center">
-      <form
-        className="max-w-sm space-y-6 rounded-md bg-white p-8 shadow-md"
-        onSubmit={handleSubmit}
-      >
-        <h2 className="mb-4 text-center text-2xl font-thin">Sign Up</h2>
-        <input
-          type="text"
-          name="name"
-          placeholder="Username"
-          value={formData.name}
-          onChange={handleChange}
-          className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
-        />
-        <select
-          className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
-          name="tags"
-          placeholder="select location tags"
-          multiple
-          onChange={(e) => addTags(e)}
+    <>
+      <NavBar />
+
+      <div className="bg-lighter-base-color flex h-screen items-center justify-center">
+        <form
+          className="max-w-sm space-y-6 rounded-md bg-white p-8 shadow-md"
+          onSubmit={handleSubmit}
         >
-          <option value="NYC">Newyork City</option>
-        </select>
-        <button
-          type="submit"
-          className="bg-darker-base-color w-full rounded-md py-2 text-white shadow-md transition-all duration-300 hover:scale-105"
-        >
-          Sign Up
-        </button>
-        {error && <h1 className="text-center text-red-500">{error}</h1>}
-        {message && <h1 className="text-center text-green-500">{message}</h1>}
-      </form>
-    </div>
+          <h2 className="mb-4 text-center text-2xl font-thin">Sign Up</h2>
+          <input
+            type="text"
+            name="name"
+            placeholder="Username"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
+          />
+          <select
+            className="w-full rounded-md border px-4 py-2 shadow-sm focus:outline-none"
+            name="tags"
+            placeholder="select location tags"
+            multiple
+            onChange={(e) => addTags(e)}
+          >
+            <option value="NYC">Newyork City</option>
+          </select>
+          <button
+            type="submit"
+            className="bg-darker-base-color w-full rounded-md py-2 text-white shadow-md transition-all duration-300 hover:scale-105"
+          >
+            Sign Up
+          </button>
+          {error && <h1 className="text-center text-red-500">{error}</h1>}
+          {message && <h1 className="text-center text-green-500">{message}</h1>}
+        </form>
+      </div>
+    </>
   );
 };
 
